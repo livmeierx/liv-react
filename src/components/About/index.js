1351080b98575712443c38d223d34639521f8b70
livmeierx <liv.meier13@gmail.com>
2022-09-18 07:30:30 -0400

    updated app/index.js and about page

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section className="container">
+            <h2 class="top-title">Liv Meier</h2>
+            <hr></hr>
+            <div>
+                <img></img>
+
+                <p>
+                    Need to add info
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
